5a5cbbcc9fb2450d1d941e643924121b2d3fc89c
joaquinaedo <jedo@udesa.edu.ar>
2024-11-16 03:42:53 -0300

    Hice las carpetas

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app'; 
+import firebase from 'firebase; '
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3xYI7j0nf19EhJArtixk-08bEJkog2uE",
+  authDomain: "clasefirebase-40c43.firebaseapp.com",
+  projectId: "clasefirebase-40c43",
+  storageBucket: "clasefirebase-40c43.firebasestorage.app",
+  messagingSenderId: "589238457132",
+  appId: "1:589238457132:web:d48fcee2d8f7da0a20d289"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
