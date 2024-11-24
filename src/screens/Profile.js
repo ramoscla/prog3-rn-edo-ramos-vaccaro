5cc25796828c30fb38b157f1e0b6a3b765d0c75c
joaquinaedo <jedo@udesa.edu.ar>
2024-11-24 17:13:57 -0300

    hice logout

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,5 @@
-import { View, Text, TextInput } from 'react-native';
 import React, { Component } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text, FlatList, TextInput } from "react-native";
 import { auth, db } from '../firebase/config';
 
 
@@ -9,18 +8,50 @@ class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            posts: [],
+            userData: [], 
+            loading: true
+
         };
+    }
+    componentDidMount(){
+    }
+
+    logout() {
+        this.props.navigation.navigate("Login")
+        auth.signOut()
+        this.setState({ loggedIn: false })
     }
 
     render() {
         return (
             <View>
                 <Text> Profile </Text>
+                <TouchableOpacity
+                    onPress={() => this.logout()}>
+                    <Text style={styles.boton}>Desloguear</Text>
+                </TouchableOpacity>
+
             </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    boton: {
+        backgroundColor: "lightblue",
+        paddingBottom: "2vh",
+        paddingTop: "2vh",
+        width: "50vw",
+        textAlign: "center",
+        borderRadius: 10,
+        marginBottom: "5%"
 
+    }});
 
 export default Profile
 
